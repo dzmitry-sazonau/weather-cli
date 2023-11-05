b03b0399ce9e14409b64994ddddd5cb9cea4481f
Dzmitry Sazonau <dzmitry.sazonau@gmail.com>
2023-11-05 17:13:27 +0100

    add handler for args

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+
+const initCLI = () => {
+  const args = getArgs(process.argv)
+
+  if (args.h) {
+
+  }
+
+  if (args.s) {
+
+  }
+
+  if (args.t) {
+
+  }
+
+
+}
+
+initCLI()
